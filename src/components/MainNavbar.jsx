279dc5394c59c1132157db9caa0c180991df53cc
suhani chauhan <suhani.chauhan@campus.dsvv.in>
2025-09-27 17:13:20 +0530

    add login

--- src/components/MainNavbar.jsx
+++ src/components/MainNavbar.jsx
@@ -1,13 +1,15 @@
+// frontend/src/components/MainNavbar.jsx
 import React from "react";
-import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { FaUser } from "react-icons/fa";
 
 export default function MainNavbar() {
   return (
-    <Navbar expand="lg" sticky="top" className="navbar-glass">
-      <Container>
-        {/* logo */}
-        <Navbar.Brand as={Link} to="/">
+    <Navbar expand="lg" sticky="top" className="navbar-glass shadow-sm">
+      <Container className="d-flex justify-content-between align-items-center">
+        {/* Left: Logo */}
+        <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
           <img
             src="/DSVV_LOGO_BLACK.png"
             alt="Dept of CS"
@@ -16,14 +18,25 @@ export default function MainNavbar() {
           />
         </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="navbar-nav" />
-        <Navbar.Collapse id="navbar-nav">
-          <Nav className="ms-auto align-items-center">
-            <Nav.Link as={Link} to="/">
-              Home
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        {/* Center: Title */}
+        <div className="mx-auto text-center d-none d-lg-block">
+          <span style={{ fontSize: "1.5rem", fontWeight: "700", color: "#000" }}>
+            News & Events
+          </span>
+        </div>
+
+        {/* Right: Admin login */}
+        <Nav className="ms-auto">
+          <Button
+            as={Link}
+            to="/admin"
+            variant="outline-dark"
+            size="sm"
+            className="d-flex align-items-center"
+          >
+            <FaUser className="me-1" /> Login
+          </Button>
+        </Nav>
       </Container>
     </Navbar>
   );
