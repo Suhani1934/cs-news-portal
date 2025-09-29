d3352d8a482e66cf8ce4f47e94ac5be4bcba8b65
suhani chauhan <suhani.chauhan@campus.dsvv.in>
2025-09-29 13:57:18 +0530

    align footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-// frontend/src/App.jsx
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import EventDetail from './pages/EventDetail';
-import MainNavbar from './components/MainNavbar';
-import AdminLogin from './admin/AdminLogin';
-import AdminDashboard from './admin/AdminDashboard';
-import Footer from './components/Footer';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import EventDetail from "./pages/EventDetail";
+import MainNavbar from "./components/MainNavbar";
+import AdminLogin from "./admin/AdminLogin";
+import AdminDashboard from "./admin/AdminDashboard";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
       <MainNavbar />
-      <div className="container mt-4">
+
+      {/* Main content grows to fill space */}
+      <div className="flex-grow-1 container mt-4">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/events/:id" element={<EventDetail />} />
@@ -20,7 +21,8 @@ export default function App() {
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
         </Routes>
       </div>
+
       <Footer />
-    </>
+    </div>
   );
 }
